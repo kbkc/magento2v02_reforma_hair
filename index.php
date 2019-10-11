ba78ee38a83527dd71524a0e0be9dc0cb687c6e9
kbkc <mposohov@hotmail.com>
2019-10-11 22:23:57 +0200

    add ua and ru url to index php

--- index.php
+++ index.php
@@ -33,7 +33,24 @@ HTML;
     exit(1);
 }
 
-$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
-/** @var \Magento\Framework\App\Http $app */
-$app = $bootstrap->createApplication(\Magento\Framework\App\Http::class);
+$params = $_SERVER;
+$domain2store = array(
+    'mg2.1stcollection.com'=>'main_website_store', // Replace your Website, Store or Storeview code with this.
+    'mg2.1stcollection.com'=>'sv_uk',      // Replace your Website, Store or Storeview code with this.
+    'reforma-romania.com'=>'sv_ua',      // Replace your Website, Store or Storeview code with this.
+    'reforma-portugal.com'=>'sv_ru',      // Replace your Website, Store or Storeview code with this.
+    );
+if(isset($domain2store[$_SERVER['HTTP_HOST']]))
+    $storecode = $domain2store[$_SERVER['HTTP_HOST']];
+$_SERVER['MAGE_MODE'] = 'developer';
+$params[\Magento\Store\Model\StoreManager::PARAM_RUN_CODE] = isset($storecode) ? $storecode : '';
+$params[\Magento\Store\Model\StoreManager::PARAM_RUN_TYPE] = 'store';
+$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $params);
+$app = $bootstrap->createApplication('Magento\Framework\App\Http');
 $bootstrap->run($app);
+
+
+#$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
+/** @var \Magento\Framework\App\Http $app */
+#$app = $bootstrap->createApplication(\Magento\Framework\App\Http::class);
+#$bootstrap->run($app);
